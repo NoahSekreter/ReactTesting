a866957b4eb9ede73ba1e8851ed5de74051b44ff
Noah Sekreter <nsekreter@gmail.com>
2021-08-06 14:11:30 -0500

    Learned jsx mapping

--- src/index.js
+++ src/index.js
@@ -15,41 +15,66 @@ import './index.css';
 // const author = "Bram Stoker"
 // img = 'https://youngentertainmentmag.com/wp-content/uploads/2015/09/dracula-book-cover-e1368750274302.jpg'
 
-const firstBook = {
+const books = [{
   title: 'Dracula',
   author: 'Bram Stoker',
   img: 'https://youngentertainmentmag.com/wp-content/uploads/2015/09/dracula-book-cover-e1368750274302.jpg'
-}
-
-const secondBook = {
+}, {
   title: 'Pride and Prejudice',
   author: 'Jane Austen',
   img: 'https://d28hgpri8am2if.cloudfront.net/book_images/onix/cvr9781499806250/pride-and-prejudice-9781499806250_hr.jpg'
-}
+}, {
+  title: 'The Jewish-Japanese Sex & Cookbook and How to Raise Wolves',
+  author: 'Jack Douglas',
+  img: 'https://images-na.ssl-images-amazon.com/images/I/51XS6q8o0ML._SX218_BO1,204,203,200_QL40_.jpg'
+}]
+
+// const firstBook = {
+//   title: 'Dracula',
+//   author: 'Bram Stoker',
+//   img: 'https://youngentertainmentmag.com/wp-content/uploads/2015/09/dracula-book-cover-e1368750274302.jpg'
+// }
+
+// const secondBook = {
+//   title: 'Pride and Prejudice',
+//   author: 'Jane Austen',
+//   img: 'https://d28hgpri8am2if.cloudfront.net/book_images/onix/cvr9781499806250/pride-and-prejudice-9781499806250_hr.jpg'
+// }
 
 function BookList() {
+  // The books.map is how you run through an array in jsx
   return (
     <section className="booklist">
-      <Book title={firstBook.title} author={secondBook.author} img={firstBook.img} />
+      {books.map((book) => {
+        // const { title, author, img } = book;
+        return <Book book={book}></Book>;
+      })}
+      {/* <Book title={firstBook.title} author={firstBook.author} img={firstBook.img}>
+        <p>lorem ipsum</p>
+      </Book>
       <Book
         title={secondBook.title}
         author={secondBook.author}
         img={secondBook.img}
       />
       <Book title='Dracula' author='Bram Stoker' img={firstBook.img} />
-      <Book title='Pride and Prejudice' author='Jane Austen' img={secondBook.img} />
+      <Book title='Pride and Prejudice' author='Jane Austen' img={secondBook.img} /> */}
     </section>
   );
 }
 
+// You can put in { title, author, img } in place of props to get the same results as the const below
+// You can place anything within an object by placing the "children" variable
 const Book = (props) => {
-  console.log(props)
+  // console.log(props)
   // When returning javascript, use {} to write functions into it.
+  // This below saves us the trouble of putting in "props." before every variable
+  const { title, author, img, children } = props.book
   return <article className="book">
-    <img src={props.img} alt={props.title + ', by ' + props.author} width="100px" />
-    <h3>{props.title}</h3>
-    <h4>{props.author}</h4>
-
+    <img src={img} alt={title + ', by ' + author} width="200px" />
+    <h3>{title}</h3>
+    <h4>{author}</h4>
+    {children}
   </article>
 }
 
